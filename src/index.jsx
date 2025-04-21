fdc922c2b21a63b8a99cd405350948c06d62031f
esclickteam <joinesclick@gmail.com>
2025-04-21 19:01:32 +0300

    Wrap entire App in AuthProvider and BrowserRouter in src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,17 @@
 // src/index.jsx
--import React from "react";
--import ReactDOM from "react-dom/client";
--import App from "./App";
--
--ReactDOM.createRoot(document.getElementById("root")).render(
--  <React.StrictMode>
--    <App />
--  </React.StrictMode>
--);
-+import React from "react";
-+import ReactDOM from "react-dom/client";
-+import { BrowserRouter } from "react-router-dom";
-+import App from "./App";
-+import { AuthProvider } from "./context/AuthContext";
-+
-+ReactDOM.createRoot(document.getElementById("root")).render(
-+  <React.StrictMode>
-+    <AuthProvider>
-+      <BrowserRouter>
-+        <App />
-+      </BrowserRouter>
-+    </AuthProvider>
-+  </React.StrictMode>
-+);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
+);
