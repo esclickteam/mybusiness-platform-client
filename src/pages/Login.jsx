b9640eda6ebac2ec50e5c0805f3168a0b853a0f4
esclickteam <joinesclick@gmail.com>
2025-04-23 14:15:42 +0300

    תיקון נתיב ייבוא useAuth ב-Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "../context/AuthContext";
 import "../../styles/Login.css";
 import ForgotPassword from "./ForgotPassword";
 
