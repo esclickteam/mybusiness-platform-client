87128a6c93090b69dab23662ff6fc46748a1deab
esclickteam <joinesclick@gmail.com>
2025-04-24 18:02:22 +0300

    תיקון שגיאת import: עדכון נתיב לBuild.css

--- src/components/shared/BusinessProfileView.jsx
+++ src/components/shared/BusinessProfileView.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../Build.css";
+import "../../../pages/business/dashboardPages/Build.css";
 import "../buildTabs/MainTab.css";
 
 const BusinessProfileView = ({ profileData }) => {
