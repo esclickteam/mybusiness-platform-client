81373dccd0026a28bdbf7e86adfe3057934d6c6e
esclickteam <joinesclick@gmail.com>
2025-04-25 13:19:08 +0300

    Move public business route into BusinessDashboardRoutes with layout wrapper

--- src/pages/business/BusinessDashboardRoutes.jsx
+++ src/pages/business/BusinessDashboardRoutes.jsx
@@ -4,38 +4,53 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import BusinessDashboardLayout from "./BusinessDashboardLayout";
 
-import Profile from "./dashboardPages/Profile";
-import Build from "./dashboardPages/Build";
-import DashboardPage from "./dashboardPages/DashboardPage";
-import Collab from "./dashboardPages/Collab";
-import Upgrade from "./dashboardPages/Upgrade";
-import CartPage from "./dashboardPages/buildTabs/shopAndCalendar/Appointments/CartPage";
-import ChatTab from "./dashboardPages/buildTabs/ChatTab";
-import BusinessChat from "./dashboardPages/BusinessChatComponent";
-import AffiliatePage from "./dashboardPages/AffiliatePage";
-import EsclickAdvisor from "./dashboardPages/EsclickAdvisor";
+import Build                               from "./dashboardPages/Build";
+import Profile                             from "./dashboardPages/Profile";
+import DashboardPage                       from "./dashboardPages/DashboardPage";
+import Collab                              from "./dashboardPages/Collab";
+import Upgrade                             from "./dashboardPages/Upgrade";
+import CartPage                            from "./dashboardPages/buildTabs/shopAndCalendar/Appointments/CartPage";
+import ChatTab                             from "./dashboardPages/buildTabs/ChatTab";
+import BusinessChat                        from "./dashboardPages/BusinessChatComponent";
+import AffiliatePage                       from "./dashboardPages/AffiliatePage";
+import EsclickAdvisor                      from "./dashboardPages/EsclickAdvisor";
 
-import CRMMain from "./dashboardPages/crmpages/CRMMain";
-import CRMAppointmentsTab from "./dashboardPages/crmpages/CRMAppointmentsTab";
-import CRMClientsTab from "./dashboardPages/crmpages/CRMClientsTab";
-import CRMServicesTab from "./dashboardPages/crmpages/CRMServicesTab";
-import CRMSettingsTab from "./dashboardPages/crmpages/CRMSettingsTab";
+import CRMMain                             from "./dashboardPages/crmpages/CRMMain";
+import CRMAppointmentsTab                  from "./dashboardPages/crmpages/CRMAppointmentsTab";
+import CRMClientsTab                       from "./dashboardPages/crmpages/CRMClientsTab";
+import CRMServicesTab                      from "./dashboardPages/crmpages/CRMServicesTab";
+import CRMSettingsTab                      from "./dashboardPages/crmpages/CRMSettingsTab";
 
-import BusinessMessagesPage from "./dashboardPages/BusinessMessagesPage";
-import GoalsPage from "./dashboardPages/GoalsPage";
+import BusinessMessagesPage                from "./dashboardPages/BusinessMessagesPage";
+import GoalsPage                           from "./dashboardPages/GoalsPage";
 
-import { BusinessServicesProvider } from "../../context/BusinessServicesContext";
+import { BusinessServicesProvider }       from "../../context/BusinessServicesContext";
 
 const BusinessDashboardRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<BusinessDashboardLayout />}>
-        {/* ברירת מחדל ל־DashboardPage */}
+      {/* כל התוכן בתוך ה־Layout */}
+      <Route element={<BusinessDashboardLayout />}>
+        {/* Public: עמוד העסק הציבורי עם ה־sidebar */}
+        <Route
+          path="/business/:businessId"
+          element={
+            <BusinessServicesProvider>
+              <Build />
+            </BusinessServicesProvider>
+          }
+        />
+
+        {/* ברירת מחדל ל־dashboard */}
         <Route index element={<Navigate to="dashboard" replace />} />
 
+        {/* טאב דשבורד */}
         <Route path="dashboard" element={<DashboardPage />} />
+
+        {/* טאב פרופיל */}
         <Route path="profile" element={<Profile />} />
 
+        {/* טאב עריכת עמוד */}
         <Route
           path="build"
           element={
@@ -44,6 +59,8 @@ const BusinessDashboardRoutes = () => {
             </BusinessServicesProvider>
           }
         />
+
+        {/* Cart */}
         <Route
           path="cart"
           element={
@@ -53,13 +70,26 @@ const BusinessDashboardRoutes = () => {
           }
         />
 
+        {/* Collab */}
         <Route path="collab" element={<Collab />} />
+
+        {/* Upgrade */}
         <Route path="upgrade" element={<Upgrade />} />
+
+        {/* Esclick Advisor */}
         <Route path="esclick" element={<EsclickAdvisor />} />
+
+        {/* Goals */}
         <Route path="goals" element={<GoalsPage />} />
+
+        {/* Chat */}
         <Route path="chat-test" element={<ChatTab isPreview={true} />} />
         <Route path="chat/:partnerId" element={<BusinessChat />} />
+
+        {/* Affiliate */}
         <Route path="affiliate" element={<AffiliatePage />} />
+
+        {/* Messages */}
         <Route path="messages" element={<BusinessMessagesPage />} />
 
         {/* CRM */}
