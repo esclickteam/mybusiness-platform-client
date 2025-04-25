bca76b739116a787fddbc9d09822781c79214538
esclickteam <joinesclick@gmail.com>
2025-04-25 11:57:29 +0300

    Use @components alias in BuildBusinessPage import

--- src/pages/PublicProfilePage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import BusinessBuilderPage from "../components/BusinessBuilderPage";
-
-export default function PublicProfilePage() {
-  // מצב תצוגה בלבד (publicView=true)
-  return <BusinessBuilderPage publicView={true} />;
-}
