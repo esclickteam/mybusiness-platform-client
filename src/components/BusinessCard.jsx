e757849c2dc34b68a3ce38c9169b2549d0c855d2
esclickteam <joinesclick@gmail.com>
2025-04-26 09:33:04 +0300

    Fix

--- src/components/BusinessCard.jsx
+++ src/components/BusinessCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./BusinessCard.css";
 
 const BusinessCard = ({ business }) => (
@@ -8,15 +9,32 @@ const BusinessCard = ({ business }) => (
         <img src={business.logoUrl} alt={`${business.name} logo`} />
       </div>
     )}
+
     <div className="business-card__content">
       <h2 className="business-card__title">{business.name}</h2>
+
+      {/* תיאור העסק */}
+      {business.description && (
+        <p className="business-card__description">
+          {business.description}
+        </p>
+      )}
+
       {business.category && (
         <p className="business-card__subtitle">{business.category}</p>
       )}
+
       {business.phone && (
         <p className="business-card__phone">📞 {business.phone}</p>
       )}
-      <button className="business-card__btn">צפה בפרופיל</button>
+
+      {/* כפתור קישור לפרופיל */}
+      <Link
+        to={`/business/${business._id}`}
+        className="business-card__btn"
+      >
+        צפה בפרופיל
+      </Link>
     </div>
   </div>
 );
