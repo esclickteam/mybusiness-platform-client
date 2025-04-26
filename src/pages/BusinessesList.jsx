0314a4b0620215085d11a9bfeed0f5d1b4a6ac54
esclickteam <joinesclick@gmail.com>
2025-04-26 09:24:34 +0300

    Fix

--- src/pages/BusinessesList.jsx
+++ src/pages/BusinessesList.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import API from "@api";
 import BusinessCard from '../components/BusinessCard';
 import './BusinessList.css';
+import './BusinessProfilePage.css';  // לטעינת רקע גרדיאנט
 
 const BusinessesList = () => {
   const [businesses, setBusinesses] = useState([]);
@@ -21,12 +22,14 @@ const BusinessesList = () => {
   }, []);
 
   return (
-    <div>
-      <h1>רשימת עסקים</h1>
-      <div className="business-list">
-        {businesses.map(business => (
-          <BusinessCard key={business._id} business={business} />
-        ))}
+    <div className="profile-page">
+      <div className="business-list-container">
+        <h1>רשימת עסקים</h1>
+        <div className="business-list">
+          {businesses.map(business => (
+            <BusinessCard key={business._id} business={business} />
+          ))}
+        </div>
       </div>
     </div>
   );
