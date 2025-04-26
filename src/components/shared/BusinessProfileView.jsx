3d8efaa5d4ac6ae59452ea07f6daf40b3555bf44
esclickteam <joinesclick@gmail.com>
2025-04-26 16:32:08 +0300

    Fix

--- src/components/shared/BusinessProfileView.jsx
+++ src/components/shared/BusinessProfileView.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useParams, useNavigate, Link, useLocation } from "react-router-dom";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import API from "@api";
 import "./BusinessProfileView.css";
