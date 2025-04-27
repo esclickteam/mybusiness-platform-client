5a4d0d62355cce4a488bcb01924a37ebcccca5a1
esclickteam <joinesclick@gmail.com>
2025-04-27 18:46:05 +0300

    Fix

--- src/pages/business/dashboardPages/buildTabs/buildSections/ShopSection.jsx
+++ src/pages/business/dashboardPages/buildTabs/buildSections/ShopSection.jsx
@@ -6,15 +6,26 @@ export default function ShopSection({ shopMode, setShopMode, setBusinessDetails,
   return (
     <BusinessServicesProvider>
       <div className="form-column">
-        <ShopAndCalendar isPreview={false} shopMode={shopMode} setShopMode={setShopMode} setBusinessDetails={setBusinessDetails} />
+        {/* הצגת רכיב ShopAndCalendar כשהמשתמש לא במצב preview */}
+        <ShopAndCalendar 
+          isPreview={false} 
+          shopMode={shopMode} 
+          setShopMode={setShopMode} 
+          setBusinessDetails={setBusinessDetails} 
+        />
         <button onClick={handleSave}>💾 שמור</button>
       </div>
+      
       <div className="preview-column">
         {renderTopBar()}
         <div className="phone-preview-wrapper">
           <div className="phone-frame">
             <div className="phone-body">
-              <ShopAndCalendar isPreview shopMode={shopMode} />
+              {/* הצגת תצוגה מקדימה */}
+              <ShopAndCalendar 
+                isPreview 
+                shopMode={shopMode} 
+              />
             </div>
           </div>
         </div>
