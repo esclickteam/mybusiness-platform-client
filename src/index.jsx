3e15a9d7f9eb562f242808f40bddac8d4873d66a
esclickteam <joinesclick@gmail.com>
2025-05-03 10:25:02 +0300

    Fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
 
-import "./styles/AccessibilityWidget.css";
+import './styles/AccessibilityWidget.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
