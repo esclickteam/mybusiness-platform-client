f7c29237c4d7665fca33977874fe8a32e681acf0
esclickteam <joinesclick@gmail.com>
2025-05-04 01:33:22 +0300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import UpdatesTicker from "./components/UpdatesTicker";
