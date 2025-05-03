4db9b7f55f6d1af0d46807f277b50824d65effe7
esclickteam <joinesclick@gmail.com>
2025-05-04 01:28:02 +0300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -70,6 +70,12 @@ const EditSiteContent = lazy(() => import("./pages/admin/EditSiteContent"));
 const ManageRoles = lazy(() => import("./pages/admin/ManageRoles"));
 const AdminPayoutPage = lazy(() => import("./pages/admin/AdminPayoutPage"));
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => window.scrollTo(0, 0), [pathname]);
+  return null;
+}
+
 export default function App() {
   return (
     <>
