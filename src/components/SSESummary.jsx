85639d8c75f1ee206b4692ee2dd7336e98330b28
esclickteam <joinesclick@gmail.com>
2025-05-04 20:37:01 +0300

    Fix

--- src/components/SSESummary.jsx
+++ src/components/SSESummary.jsx
@@ -4,19 +4,24 @@ import { MdPersonAdd, MdStorefront, MdRateReview, MdInfo } from 'react-icons/md'
 import './SSESummary.css';
 
 export default function SSESummary({ updates }) {
+  // Debug: log incoming updates and types
+  console.log('SSESummary updates:', updates);
+  updates.forEach(u => console.log('  type:', u.type, 'message:', u.message || u.title));
+
   // סופרים אירועים לפי סוג, כולל מיפוי סוגים ממחרוזות שונות
   const countByType = updates.reduce((acc, u) => {
     const rawType = (u.type || '').toString().toLowerCase();
     const msg = (u.message || u.title || '').toString();
     let t;
 
-    if (/לקוח|signup|client/.test(rawType) || /לקוח/.test(msg)) {
+    if (/signup|client/.test(rawType) || /לקוח/.test(msg)) {
       t = 'client';
-    } else if (/עסק|owner|business/.test(rawType) || /עסק/.test(msg)) {
+    } else if (/owner|business/.test(rawType) || /עסק/.test(msg)) {
       t = 'business';
-    } else if (/ביקו|review/.test(rawType) || /ביקו/.test(msg)) {
+    } else if (/review/.test(rawType) || /ביקו/.test(msg)) {
       t = 'review';
     } else {
+      console.warn('SSESummary: unmapped type', u.type);
       t = 'info';
     }
 
