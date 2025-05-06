1296b7d9df46cdc2ef6c9cdabef2e288e950a511
esclickteam <joinesclick@gmail.com>
2025-05-06 12:02:22 +0300

    Fix

--- src/pages/business/dashboardPages/buildTabs/buildSections/MainSection.jsx
+++ src/pages/business/dashboardPages/buildTabs/buildSections/MainSection.jsx
@@ -3,7 +3,7 @@ import Select from "react-select";
 import { dedupeByPreview } from "../../../../../utils/dedupe";
 import rawCities from "../../../../../data/cities";
 import ALL_CATEGORIES from "../../../../../data/categories";
-import Gallery from "./Gallery";  // ייבוא הקומפוננטה החדשה
+import Gallery from "../../../components/Gallery";  // עדכון הנתיב בהתאם
 
 // הכנה של אופציות מסודרות ומסוננות
 const CITIES = Array.from(new Set(rawCities)).sort((a, b) =>
