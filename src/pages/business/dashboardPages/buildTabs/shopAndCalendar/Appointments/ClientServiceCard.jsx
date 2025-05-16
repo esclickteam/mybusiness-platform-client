e058ba5254216f53913d0d702a02eba6b7f97188
esclickteam <joinesclick@gmail.com>
2025-05-16 15:40:21 +0300

    Fix

--- src/pages/business/dashboardPages/buildTabs/shopAndCalendar/Appointments/ClientServiceCard.jsx
+++ src/pages/business/dashboardPages/buildTabs/shopAndCalendar/Appointments/ClientServiceCard.jsx
@@ -4,11 +4,10 @@ import "./ClientServiceCard.css";
 const ClientServiceCard = ({
   service,
   formatDuration,
-  onSelect // prop לפעולה בעת לחיצה
+  onSelect // action prop when clicked
 }) => {
   return (
     <div className="client-service-card">
-      {/* הצגת תמונת השירות אם קיימת */}
       {service.imageUrl && (
         <img
           src={service.imageUrl}
@@ -16,14 +15,12 @@ const ClientServiceCard = ({
           className="client-service-card__img"
         />
       )}
-
       <h4>{service.name}</h4>
       {service.description && (
         <p className="service-description">{service.description}</p>
       )}
       {service.price && <p className="price">{service.price} ₪</p>}
       <p>⏱️ {formatDuration(service.duration)}</p>
-
       <button
         className="go-to-calendar-btn"
         onClick={() => onSelect && onSelect(service)}
