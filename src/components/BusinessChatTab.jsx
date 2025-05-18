e49d92305e474afba9912d7fe492ab5119922919
esclickteam <joinesclick@gmail.com>
2025-05-18 20:20:31 +0300

    Fix

--- src/components/BusinessChatTab.jsx
+++ src/components/BusinessChatTab.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 import API from "../api";
-import "./BusinessChatTab.css";
+import styles from "./BusinessChatTab.css";
 
 export default function BusinessChatTab({ conversationId, businessId, customerId }) {
   const [messages, setMessages] = useState([]);
