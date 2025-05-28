2d18fec0db5b800e5b2012ad62e908ff9b995966
esclickteam <joinesclick@gmail.com>
2025-05-28 12:05:08 +0300

    Fix

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-import { SSEProvider } from './context/SSEContext';
 import { SocketProvider } from './context/socketContext';
 
 // Polyfill ל-Buffer בדפדפן:
 import { Buffer } from 'buffer';
 window.Buffer = window.Buffer || Buffer;
 
-// עטוף את App ב-SSEProvider **בתוך App.jsx עצמו**
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
