4e3d86792501f5b80a8695e34a5d8588e2cce8e1
esclickteam <joinesclick@gmail.com>
2025-06-05 09:15:02 +0300

    Fix

--- src/components/DashboardCards.jsx
+++ src/components/DashboardCards.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/dashboard.css";
 
 const DashboardCards = ({ stats }) => {
+  useEffect(() => {
+    console.log("DashboardCards received stats:", stats);
+  }, [stats]);
+
   const cards = [
     {
       label: "צפיות בפרופיל",
