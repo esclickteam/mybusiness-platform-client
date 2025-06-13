72974e5221ee61e5583a508800b85268e3c518c8
esclickteam <joinesclick@gmail.com>
2025-06-14 02:38:19 +0300

    Fix

--- src/components/LoginSkeleton.jsx
+++ src/components/LoginSkeleton.jsx
@@ -2,15 +2,11 @@ import React from "react";
 
 export function LoginSkeleton() {
   return (
-    <div className="login-skeleton">
-      <div className="skeleton-title" />
-      <div className="skeleton-input" />
-      <div className="skeleton-input" />
-      <div className="skeleton-button" />
-      <div className="skeleton-extra-options">
-        <div className="skeleton-link" />
-        <div className="skeleton-button-small" />
-      </div>
+    <div className="flex flex-col gap-4 p-6 max-w-md mx-auto mt-32 animate-pulse">
+      <div className="h-6 bg-gray-300 rounded w-1/2" />
+      <div className="h-10 bg-gray-300 rounded" />
+      <div className="h-10 bg-gray-300 rounded" />
+      <div className="h-10 bg-gray-400 rounded w-1/2" />
     </div>
   );
 }
