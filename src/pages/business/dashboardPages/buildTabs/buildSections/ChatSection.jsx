2d9329a0a2977e80a6fb1e7f0a45c78766725037
esclickteam <joinesclick@gmail.com>
2025-06-27 09:20:33 +0300

    Fix

--- src/pages/business/dashboardPages/buildTabs/buildSections/ChatSection.jsx
+++ src/pages/business/dashboardPages/buildTabs/buildSections/ChatSection.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { NavLink, useParams } from "react-router-dom";
 
-export default function ChatButton() {
+export default function ChatButton({ previewContent, renderTopBar }) {
   const { businessId } = useParams();
 
   if (!businessId) return null;
 
   return (
-    <div>
-      <NavLink
-        to={`/business/${businessId}/dashboard/chat`}
-        className="edit-link-button"
-      >
-        צ'אט עם לקוחות
-      </NavLink>
-    </div>
+    <>
+      <div className="form-column">
+        <NavLink
+          to={`/business/${businessId}/dashboard/chat`}
+          className="edit-link-button"
+        >
+          צ'אט עם לקוחות
+        </NavLink>
+      </div>
+
+      <div className="preview-column">
+        {renderTopBar && renderTopBar()}
+        {previewContent}
+      </div>
+    </>
   );
 }
