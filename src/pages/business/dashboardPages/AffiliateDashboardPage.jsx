5b5fbf9aee0cb29100b98905bcc2ed0ef19f3b6c
esclickteam <joinesclick@gmail.com>
2025-07-11 13:29:24 +0300

    Fix

--- src/pages/business/dashboardPages/AffiliateDashboardPage.jsx
+++ src/pages/business/dashboardPages/AffiliateDashboardPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../../context/AuthContext";
 import API from "@api";
 import "./AffiliatePage.css";
 import BankDetailsForm from "./BankDetailsForm";
