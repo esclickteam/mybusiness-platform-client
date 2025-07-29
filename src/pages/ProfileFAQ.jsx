6e52e33f3dd3f9e22291eb0a76eddd01624de2e8
esclickteam <joinesclick@gmail.com>
2025-07-29 15:27:37 +0300

    Fix

--- src/pages/ProfileFAQ.jsx
+++ src/pages/ProfileFAQ.jsx
@@ -204,7 +204,7 @@ export default function ProfileFAQ() {
             style={{
               width: "100%",
               textAlign: "right",
-              background: "#f5f5f5",
+              background: "rgba(213, 194, 255, 0.5)",
               border: "none",
               padding: "12px 15px",
               fontSize: 18,
