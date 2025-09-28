25780f16aae595b8810301b075b1d65e8646deef
esclickteam <joinesclick@gmail.com>
2025-09-28 17:07:15 +0300

    Fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import "./Home.css"; 
+import "../styles/Home.css";
 import { Link } from "react-router-dom";
 
 export default function Home() {
