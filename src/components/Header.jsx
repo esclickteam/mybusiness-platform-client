e9763720d6f68cf8e032379b79cdffbfbf1ecae3
esclickteam <joinesclick@gmail.com>
2025-09-29 14:01:21 +0300

    Fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import logo from "../images/logo.png";
+import logo from "../images/logo.jpg";
 import {
   FaBars,
   FaChevronLeft,
