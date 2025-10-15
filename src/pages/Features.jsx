ef254e39f629adaee3193773d3f0e124cf2c9c16
esclickteam <joinesclick@gmail.com>
2025-10-15 10:16:09 +0300

    Fix

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
-import "../styles/Features.css";
+import "./Features.css";
+
 
 function Features() {
   return (
