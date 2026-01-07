33611c19f4191da0f5614750eb8667b3271034ae
esclickteam <your-email@example.com>
2026-01-07 17:51:52 +0200

    הודעת קומית

--- src/pages/admin/AdminLayout.jsx
+++ src/pages/admin/AdminLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet, useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import "./AdminLayout.css";
 
 export default function AdminLayout() {
