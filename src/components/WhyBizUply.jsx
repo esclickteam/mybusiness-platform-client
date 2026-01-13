b71aac5da52c54f632d3a2ad76e49d44a93874f8
esclickteam <joinesclick@gmail.com>
2026-01-13 13:03:25 +0200

    תיאור השינוי

--- src/components/WhyBizUply.jsx
+++ src/components/WhyBizUply.jsx
@@ -3,7 +3,7 @@ export default function WhyBizUply() {
     <section className="why">
       <h2>Why Bizuply</h2>
       <ul>
-        <li>One platform instead of many tools</li>
+         <li>One platform instead of many tools</li>
         <li>Built for real daily business work</li>
         <li>Designed to grow with you</li>
       </ul>
