fc973954fc3e80dc8bd4ac17c18c4770f9f4f7c7
esclickteam <joinesclick@gmail.com>
2026-01-16 11:02:14 +0200

    תיאור השינוי

--- src/pages/TrialEndedPage.jsx
+++ src/pages/TrialEndedPage.jsx
@@ -1,14 +1,17 @@
 // TrialEndedPage.jsx
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import UpgradeOfferCard from "../components/UpgradeOfferCard/UpgradeOfferCard";
 import "./TrialEndedPage.css";
 
 export default function TrialEndedPage() {
+  const navigate = useNavigate();
+
   return (
     <div className="trial-ended-wrapper">
       <UpgradeOfferCard
-        onUpgrade={() => (window.location.href = "/pricing")}
-        onHome={() => (window.location.href = "/")}
+        onUpgrade={() => navigate("/pricing")}
+        onHome={() => navigate("/")}
       />
     </div>
   );
